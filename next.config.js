f9e43e47962aebf0d41cf489d6533ef46cf6452f
Pratik Pednekar <pratik_pednekar@mcafee.com>
2022-08-07 23:35:58 +0530

    img domain

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['localhost','contenhub.com'],
+  },
   reactStrictMode: true,
   async redirects() {
     return [
